23f7256383f174fd49d753b2fc7397202e2044e8
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2022-11-27 20:38:54 +0200

    Create auth.js

---
+++ js/Auth/auth.js
@@ -0,0 +1,19 @@
+class Auth{
+    constructor(){
+        //create validation script
+         document.querySelector("body").style.display="none";
+        const auth= localStorage.getItem("auth");
+        this.validateAuthg(auth);
+    }
+    validateAuthg(auth){
+        if(auth != 1){
+            window.location.replace("/HTML");
+        }else {
+            document.querySelector("body").style.display="block";
+        }
+    }
+    logout(){
+        localStorage.removeItem("auth");
+        window.location.replace("/index.html");
+    }
+}
